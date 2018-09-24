7a380ab5cbd255c455d048725eeef3265c1156da
nightwarrior-xxx <amanv1999@gmail.com>
2018-09-24 06:38:10 +0530

    added some changes to UI

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ Display.grid(columnspan=4)
 b7=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='7').grid(row=1,column=0)
 b8=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='8').grid(row=1,column=1)
 b8=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='9').grid(row=1,column=2)
-bC=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='clear',bg='purple').grid(row=1,column=3)
+bC=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='clear',bg='green').grid(row=1,column=3)
 #Row2
 b4=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='4').grid(row=2,column=0)
 b4=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='5').grid(row=2,column=1)
@@ -21,8 +21,13 @@ b2=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='2'
 b3=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='3').grid(row=3,column=2)
 bminus=Button(root,padx=69,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='-',bg='purple').grid(row=3,column=3)
 #Row4
-b0=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='0').grid(row=4,column=0)
-bdot=Button(root,padx=35,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='.',bg='purple').grid(row=4,column=1)
-bdivison=Button(root,padx=35,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='/',bg='purple').grid(row=4,column=2)
-bmultiply=Button(root,padx=64,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='*',bg='purple').grid(row=4,column=3)
+b_dot=Button(root,padx=35,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='.',bg='purple').grid(row=4,column=0)
+b0=Button(root,padx=30,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='0').grid(row=4,column=1)
+b_divison=Button(root,padx=35,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='/',bg='purple').grid(row=4,column=2)
+b_multiply=Button(root,padx=65,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='*',bg='purple').grid(row=4,column=3)
+#Row5
+
+b_open_bracket=Button(root,padx=34,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='(',bg='purple').grid(row=5,column=0)
+b_close_bracket=Button(root,padx=34,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text=')',bg='purple').grid(row=5,column=1)
+b_equal=Button(root,padx=114,pady=15,bd=5,fg='black',font=('arial',30,'bold'),text='=',bg='green').grid(row=5,column=2,columnspan=2)
 root.mainloop()
